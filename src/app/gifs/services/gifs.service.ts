3c93967774f2777a91225d8b6858c2abb88ee95a
DiegoJojoa <diegojojoa-94@hotmail.com>
2023-02-25 18:10:11 -0500

    gifs are captured from the Giphy API and displayed

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+  private apiKey: string = "8TJk5jC8akT4BThr2mzvCeQFBJHSkSXO";
   private _historial: string[] = [];
+
+  //TODO: Cambiar any por su tipo correspondiente
+  public resultados: any[] = [];
+
+  constructor(private http: HttpClient){}
 
   get historial(){
     return [...this._historial]
@@ -15,6 +22,13 @@ export class GifsService {
     if(!this._historial.includes(query)){
       this._historial.unshift(query);
     }
+
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=8TJk5jC8akT4BThr2mzvCeQFBJHSkSXO&q=${query}&limit=10`)
+      .subscribe((resp: any) =>{
+        console.log(resp.data);
+        this.resultados = resp.data;
+      })
+
     this._historial = this._historial.splice(0,10);
     console.log(this._historial);
   }
