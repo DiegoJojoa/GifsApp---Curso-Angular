afb64f8062f7a00bfb9e4ecf825084d4d4f9c0da
DiegoJojoa <diegojojoa-94@hotmail.com>
2023-02-25 19:21:31 -0500

    send params in request API Giphy

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,18 +1,19 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GifsService {
-  private apiKey: string = "8TJk5jC8akT4BThr2mzvCeQFBJHSkSXO";
+  private apiKey: string = '8TJk5jC8akT4BThr2mzvCeQFBJHSkSXO';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
 
   //TODO: Cambiar any por su tipo correspondiente
   public resultados: Gif[] = [];
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient) {
     this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
     this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
     // if(localStorage.getItem('historial')){
@@ -20,24 +21,30 @@ export class GifsService {
     // }
   }
 
-  get historial(){
-    return [...this._historial]
+  get historial() {
+    return [...this._historial];
   }
 
-  buscarGifs(query: string = ''){
+  buscarGifs(query: string = '') {
     query = query.trim().toLowerCase();
-    if(!this._historial.includes(query)){
+    if (!this._historial.includes(query)) {
       this._historial.unshift(query);
     }
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=8TJk5jC8akT4BThr2mzvCeQFBJHSkSXO&q=${query}&limit=10`)
-      .subscribe((resp: SearchGifsResponse) =>{
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
+
+    this.http
+      .get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params: params })
+      .subscribe((resp: SearchGifsResponse) => {
         console.log(resp.data);
         this.resultados = resp.data;
-        localStorage.setItem('resultados',JSON.stringify(this.resultados));
-      })
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
+      });
 
-    this._historial = this._historial.splice(0,10);
-    localStorage.setItem('historial',JSON.stringify(this._historial));
+    this._historial = this._historial.splice(0, 10);
+    localStorage.setItem('historial', JSON.stringify(this._historial));
   }
 }
